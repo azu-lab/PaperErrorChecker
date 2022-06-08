8cb578b2777917009c057a4185e59490783c3d7e
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-06-08 17:36:21 +0900

    remove tests

--- tests/func/conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-
-@pytest.fixture
-def sample_dir():
-    yield "C:\\Users\\atsushi\\Documents\\workspace\\env\\PEC\\Paper_Error_Checker\\tests\\sample_dir"
--- tests/func/test_run_check.py
+++
@@ -1,5 +0,0 @@
-from src.run_check import main
-
-
-def test_main(sample_dir):
-    main(sample_dir)
