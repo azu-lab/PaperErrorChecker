b7bd43301a20bb271b113af28a7e0626b75c3eb1
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-07-02 10:02:19 +0900

    temp finish

--- tests/check_lists/test_common.py
+++ tests/check_lists/test_common.py
@@ -10,3 +10,26 @@ def test_cite(mocker, capfd):
                      "common", r"\cite{X}, \cite{Y} -> \cite{X, Y}"))
     Checker(f"{os.path.dirname(__file__)}/test_common", "")
     out, err = capfd.readouterr()
+    assert "| 1 |" in out
+    assert "| 2 |" in out
+    assert "| 3 |" in out
+    assert "| 4 |" in out
+
+
+def test_insert_a_period(mocker, capfd):
+    mocker.patch("src.run_checker.Checker._get_check_lists",
+                 return_value=get_one_check_dict(
+                     "common", r"Insert a period"))
+    Checker(f"{os.path.dirname(__file__)}/test_common", "")
+    out, err = capfd.readouterr()
+    assert "| 7 |" not in out
+    assert "| 8 |" in out
+    assert "| 9 |" in out
+    assert "| 10 |" not in out
+    assert "| 11 |" in out
+    assert "| 12 |" in out
+    assert "| 13 |" in out
+    assert "| 14 |" in out
+    assert "| 15 |" not in out
+    assert "| 16 |" in out
+    assert "| 17 |" in out
